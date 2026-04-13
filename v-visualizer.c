077ca903baacefe27adec6842947e5468a4f3199
Gustavo <gustavoalima14057@gmail.com>
2026-04-12 23:42:58 -0300

    define standard

--- v-visualizer.c
+++ v-visualizer.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
+#define ARRLEN 5
+
 
 void vArr(int a[]);
 int maxColHeigh(int a[]);
 
 int main(){
-		int arr1[] = {0, 1, 2, 3, 4};
-	
+	int arr1[ARRLEN] = {0, 1, 2, 3, 4};
 	vArr(arr1);
 
-
 	return 0;
-
 }
 
 void vArr(int a[]){
-	char line[5];
+	char line[ARRLEN];
 	int max = maxColHeigh(a);
 	for (max; max > 0; max--){
-		for(int i = 0; i < 5; i++){
+		for(int i = 0; i < ARRLEN; i++){
 			line[i] = a[i] >= max ? '#' : ' ';
-		
 		}
 		printf("%s\n", line);
 	}
-
-
 }
 
 int maxColHeigh(int a[]){
 	int m = 0;
-	for (int i = 0; i < 5; i++){
+	for (int i = 0; i < ARRLEN; i++){
 		if (a[i] > m)
 			m = a[i];
 	}
